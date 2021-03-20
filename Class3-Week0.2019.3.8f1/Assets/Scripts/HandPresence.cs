d16cafcc92c6c55196a632c14cf4859a80103625
mtfallsVR <mtfallsVR@atemosta.com>
2021-03-19 21:36:12 -0400

    Add Hands to Controller

--- Class3-Week0.2019.3.8f1/Assets/Scripts/HandPresence.cs
+++ Class3-Week0.2019.3.8f1/Assets/Scripts/HandPresence.cs
@@ -5,10 +5,14 @@ using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
+    public bool showController = false;
     public InputDeviceCharacteristics controllerCharacteristics;
     private InputDevice targetDevice;
+    public GameObject handModelPrefab;
+
     public List<GameObject> controllerPrefabs;
     private GameObject spawnedController;
+    private GameObject spawnedHandModel;
 
     // Start is called before the first frame update
     void Start()
@@ -34,17 +38,23 @@ public class HandPresence : MonoBehaviour
                 Debug.Log("Did not find corresponding controller model");
                 spawnedController = Instantiate(controllerPrefabs[0], transform);
             }
+
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue))
-            Debug.Log("Pressing Primary Button");
-        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
-            Debug.Log("Trigger pressed:" + triggerValue);
-        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
-            Debug.Log("Primary Touchpad" + primary2DAxisValue);
+        if (showController)
+        {
+            spawnedHandModel.SetActive(false);
+            spawnedController.SetActive(true);
+        }
+        else
+        {
+            spawnedHandModel.SetActive(true);
+            spawnedController.SetActive(false);
+        }
     }
 }
