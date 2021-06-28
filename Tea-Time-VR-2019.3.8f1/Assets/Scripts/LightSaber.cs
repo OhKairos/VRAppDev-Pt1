962c5a68a0292f28e3b5ca9c0e60e40137fb04a8
Oscar Kevin <mtfallsVR@atemosta.com>
2021-06-28 19:01:51 -0400

    Lightsaber Turn On Animation

---
+++ Tea-Time-VR-2019.3.8f1/Assets/Scripts/LightSaber.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSaber : MonoBehaviour
+{
+    public Animator LightSaberAnimator;
+
+    // Start is called before the first frame update
+    public void TurnOnSaber() {
+        LightSaberAnimator.SetTrigger("Turn Lightsaber On");
+    }
+
+}
