ddf8ceafd95a6987956c039c2ccb336d1d41de0c
Oscar Kevin <mtfallsVR@atemosta.com>
2021-06-28 19:50:04 -0400

    Part 2 Week 3 - Light Saber (#6)
    * Add Lightsaber Prefab
    
    * Add Volumetric Lines and Bloom Intensity
    
    * Lightsaber Turn On Animation
    
    * Lightsaber Turning Off Animation
    
    * Add Box Collider to Volumetric Light
    
    * Add Sword Audio Effects

---
+++ Tea-Time-VR-2019.3.8f1/Assets/Scripts/LightSaber.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSaber : MonoBehaviour
+{
+    public Animator LightSaberAnimator;
+    public AudioSource audioSource;
+    public AudioClip beamOn;
+    public AudioClip beamOff;
+
+    // Start is called before the first frame update
+    public void TurnOnSaber() {
+        audioSource.PlayOneShot(beamOn);
+        LightSaberAnimator.SetTrigger("Turn Lightsaber On");
+        LightSaberAnimator.ResetTrigger("Turn Lightsaber Off");
+    }
+
+    public void TurnOffSaber()
+    {
+        audioSource.PlayOneShot(beamOff);
+        LightSaberAnimator.SetTrigger("Turn Lightsaber Off");
+        LightSaberAnimator.ResetTrigger("Turn Lightsaber On");
+
+    }
+
+}
