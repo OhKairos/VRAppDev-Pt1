95386492975760da31d51f7a146e815b98a8ccb5
Oscar Kevin <mtfallsVR@atemosta.com>
2021-06-28 19:49:23 -0400

    Add Sword Audio Effects

--- Tea-Time-VR-2019.3.8f1/Assets/Scripts/LightSaber.cs
+++ Tea-Time-VR-2019.3.8f1/Assets/Scripts/LightSaber.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class LightSaber : MonoBehaviour
 {
     public Animator LightSaberAnimator;
+    public AudioSource audioSource;
+    public AudioClip beamOn;
+    public AudioClip beamOff;
 
     // Start is called before the first frame update
     public void TurnOnSaber() {
+        audioSource.PlayOneShot(beamOn);
         LightSaberAnimator.SetTrigger("Turn Lightsaber On");
         LightSaberAnimator.ResetTrigger("Turn Lightsaber Off");
     }
 
     public void TurnOffSaber()
     {
+        audioSource.PlayOneShot(beamOff);
         LightSaberAnimator.SetTrigger("Turn Lightsaber Off");
         LightSaberAnimator.ResetTrigger("Turn Lightsaber On");
+
     }
 
 }
