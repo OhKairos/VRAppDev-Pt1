15a6888f69cf754a59ed2c3931fdcfdc6d355238
Oscar Kevin <mtfallsVR@atemosta.com>
2021-07-28 22:33:05 -0400

    Consolidate Unity Project Repos  (#7)
    * consolidate github repos
    
    * Update README.md
    
    * Update README.md

---
+++ ASVR-Kits/Assets/Plugins/uploadKit.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "kit[zip]=@C:\Users\Myia\Documents\GitHub\ASVR-Kits\ASVR-Kits\1776547302412911298_sao-plat-ed.zip" -F "kit[game_engine_version]=20194" https://account.altvr.com/api/kits/1776547302412911298.json
